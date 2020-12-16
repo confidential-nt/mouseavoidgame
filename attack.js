f2cdeffec7fece2f1e439bbaf1716d99bd265f66
confidential-nt <youyoy689@gmail.com>
2020-12-17 06:56:42 +0900

    Added draw attacks features

--- attack.js
+++ attack.js
@@ -1,8 +1,8 @@
-import {getAttackDirection} from "./direction.js";
-import {MAZE_SIZE_X} from "./maze.js";
+import { getAttackDirection } from "./direction.js";
+import { MAZE_SIZE_X } from "./maze.js";
 
 const rows = document.querySelectorAll(".row");
-const firstAttackRow =  rows[1];
+const firstAttackRow = rows[1];
 const secondAttackRow = rows[7];
 const lastAttackRow = rows[rows.length - 1];
 const firstAttackPos = firstAttackRow.querySelector(".column");
@@ -10,79 +10,73 @@ const secondAttackPos = secondAttackRow.querySelector(".column");
 const lastAttackPos = lastAttackRow.querySelector(".column");
 
 let start = false;
+let create = false; //얘는 나중에 한분기 끝나고 다른 한분기 시작하게 할때 이용할 것.
 
 let attacks = [
-    {
-        id: 1,
-        x: 0,
-        firstPos: firstAttackPos,
-    },
-    {
-        id: 2,
-        x: 0,
-        firstPos: secondAttackPos,
-    },
-    {
-        id: 3,
-        x: 0,
-        firstPos: lastAttackPos,
-    }
-]
+  {
+    id: 1,
+    x: 0,
+  },
+  {
+    id: 2,
+    x: 0,
+  },
+  {
+    id: 3,
+    x: 0,
+  },
+];
 
 const ATTACK_SPEED = 2;
 
 let lastRenderTime = 0;
 
+export function drawAttack(timestamp) {
+  const sec = Math.round(timestamp / 1000);
 
-
-
-export function handleAttack(timestamp){
-    const randomNum = Math.floor( Math.random() * 3) + 1;
-    switch (randomNum){
-        case 1:
-            const firstAttack = attacks.find(el => el["id"] === 1);
-            updateAttack(firstAttack, timestamp);
-            drawAttack(firstAttack);
-            break;
-        case 2:
-            const secondAttack = attacks.find(el => el["id"] === 2);
-            updateAttack(secondAttack, timestamp);
-            drawAttack(secondAttack);
-            break;
-        case 3:
-            const thirdAttack = attacks.find(el => el["id"] === 3);
-            updateAttack(thirdAttack, timestamp);
-            drawAttack(thirdAttack);
-    }
+  if (create) return;
+  const attack = document.createElement("div");
+  attack.classList.add("attack");
+  const attackElements = document.querySelectorAll(".attack");
+  switch (sec) {
+    case 2:
+      if (firstAttackPos.contains(attackElements[0])) return;
+      firstAttackPos.appendChild(attack);
+      break;
+    case 3:
+      if (secondAttackPos.contains(attackElements[1])) return;
+      secondAttackPos.appendChild(attack);
+      break;
+    case 4:
+      if (lastAttackPos.contains(attackElements[2])) return;
+      lastAttackPos.appendChild(attack);
+      create = true;
+      break;
+  }
 }
 
+// export function drawAttack() {
+//   const firstAttackRowCol = firstAttackRow.querySelectorAll(".column");
+//   if (!start) {
+//     firstDrawAttack();
+//     return;
+//   }
+//   if (attack.x > MAZE_SIZE_X) {
+//     start = !start;
+//     const lastPosition = firstAttackRowCol[firstAttackRowCol.length - 1];
+//     const lastAttack = lastPosition.querySelector(".attack");
+//     lastPosition.removeChild(lastAttack);
+//     attack.x = 0;
+//   }
 
+//   const attackElement = document.querySelector(".attack");
 
-export function drawAttack(){
-    const firstAttackRowCol = firstAttackRow.querySelectorAll(".column");
-    if(!start){
-        firstDrawAttack();
-        return;
-    }
-    if(attack.x > MAZE_SIZE_X){
-        start = !start;
-        const lastPosition = firstAttackRowCol[firstAttackRowCol.length - 1]
-        const lastAttack = lastPosition.querySelector(".attack");
-        lastPosition.removeChild(lastAttack);
-        attack.x = 0;
-    }
-    
-    const attackElement = document.querySelector(".attack");
-    
-    firstAttackRowCol[attack.x].appendChild(attackElement);
-    
+//   firstAttackRowCol[attack.x].appendChild(attackElement);
+// }
 
-}
-
-
-export function updateAttack(object,timestamp){
-    let progress = (timestamp - lastRenderTime) / 1000;
-    if(progress < 1 / ATTACK_SPEED) return;
-    lastRenderTime = timestamp;
-    object.x = getAttackDirection();
+export function updateAttack(object, timestamp) {
+  let progress = (timestamp - lastRenderTime) / 1000;
+  if (progress < 1 / ATTACK_SPEED) return;
+  lastRenderTime = timestamp;
+  object.x = getAttackDirection();
 }
--- game.js
+++ game.js
@@ -1,8 +1,14 @@
-import {draw as drawBall, update as updateBall, bumpIntoMaze, ball as ballLoc, onFinishLine} from "./ball.js";
-import {outSideOfMaze} from "./maze.js";
-import {drawGem, eatGem} from "./gem.js";
-import {timer, timeOut, timerId} from "./time.js"
-import { handleAttack} from "./attack.js";
+import {
+  draw as drawBall,
+  update as updateBall,
+  bumpIntoMaze,
+  ball as ballLoc,
+  onFinishLine,
+} from "./ball.js";
+import { outSideOfMaze } from "./maze.js";
+import { drawGem, eatGem } from "./gem.js";
+import { timer, timeOut, timerId } from "./time.js";
+import { drawAttack } from "./attack.js";
 
 const failConfirm = document.querySelector(".failConfirm");
 const finishConfirm = document.querySelector(".finishConfirm");
@@ -15,74 +21,67 @@ let iseatGem = false;
 let score = 0;
 let justAgo = false;
 
-
 scoreBoard.innerText = `left gems:${ALL_GEMS}`;
 
-function main(timestamp){
-    if(gameOver){
-        failConfirm.classList.remove("hide");
-        clearInterval(timerId);
-        return;
-    }
-    if(finish){
-        finishConfirm.classList.remove("hide");
-        clearInterval(timerId);
-        return;
-    }
-    if(!justAgo){
-        const startTime = Date.now();
-        timer(startTime);
-        justAgo = true;
-    }
-    window.requestAnimationFrame(main);
-    updateBall();
-    checkDeath();
-    checkFinish();
-    if(gameOver || finish) return;
-    drawBall();
-    drawGem();
-    getScore();
-    handleAttack(timestamp);
+function main(timestamp) {
+  if (gameOver) {
+    failConfirm.classList.remove("hide");
+    clearInterval(timerId);
+    return;
+  }
+  if (finish) {
+    finishConfirm.classList.remove("hide");
+    clearInterval(timerId);
+    return;
+  }
+  if (!justAgo) {
+    const startTime = Date.now();
+    timer(startTime);
+    justAgo = true;
+  }
+  window.requestAnimationFrame(main);
+  updateBall();
+  checkDeath();
+  checkFinish();
+  if (gameOver || finish) return;
+  drawBall();
+  drawGem();
+  drawAttack(timestamp);
+  getScore();
 }
 
-
-function checkDeath(){
-    const ball = document.querySelector(".ball");
-    gameOver = bumpIntoMaze(ball) || outSideOfMaze(ballLoc) || timeOut();
+function checkDeath() {
+  const ball = document.querySelector(".ball");
+  gameOver = bumpIntoMaze(ball) || outSideOfMaze(ballLoc) || timeOut();
 }
 
-function checkFinish(){
-    const ball = document.querySelector(".ball");
-    if(!ball) return;
-    if(score !== 3) return;
-    finish = onFinishLine(ball)
-    
+function checkFinish() {
+  const ball = document.querySelector(".ball");
+  if (!ball) return;
+  if (score !== 3) return;
+  finish = onFinishLine(ball);
 }
 
-function handleRestart(){
-    if(this.value == "yes"){
-        window.location = "./index.html";
-    }else {
-        failConfirm.classList.add("hide");
-        finishConfirm.classList.add("hide");
-    }
+function handleRestart() {
+  if (this.value == "yes") {
+    window.location = "./index.html";
+  } else {
+    failConfirm.classList.add("hide");
+    finishConfirm.classList.add("hide");
+  }
 }
 
-function getScore(){
-    const ball = document.querySelector(".ball");
-    const eatSound = document.querySelector(`audio[data-sound="eatGem"]`)
-    iseatGem = eatGem(ball);
+function getScore() {
+  const ball = document.querySelector(".ball");
+  const eatSound = document.querySelector(`audio[data-sound="eatGem"]`);
+  iseatGem = eatGem(ball);
 
-    if(iseatGem){
-        score++;
-        scoreBoard.innerText = `left gems:${ALL_GEMS - score}`;
-        eatSound.play();
-    }
-    
+  if (iseatGem) {
+    score++;
+    scoreBoard.innerText = `left gems:${ALL_GEMS - score}`;
+    eatSound.play();
+  }
 }
-
 
 window.requestAnimationFrame(main);
-btns.forEach(btn => btn.addEventListener("click",handleRestart))
-
-
+btns.forEach((btn) => btn.addEventListener("click", handleRestart));
